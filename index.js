f969630866fe46e8c75c2388e18f406c420f2718
abellanosa123 <caloythepro@gmail.com>
2024-10-21 07:26:20 +0800

    E lahi lahi ang toutes sa user, employee ug admin

--- index.js
+++ index.js
@@ -2,28 +2,42 @@ const express = require('express')
 const app = express()
 
 
+
+
+///USER ROUTES//////
+
 app.get('/reservation', (req, res) => {
   res.send('Hello World!')
-})
+});
+const Id = req.body.Bookingid;
+
+user.deleteOne({ BookingId : Id}).then(item => {
+  res.send('Hello World!')
+});
 
 app.post('/reservation/reserve', (req, res) => {
     res.send('Hello World!')
-  })
+  });
 
 app.get('/reservation/employee', (req, res) => {
   
-})
+});
 app.post('/reservation/addemployee', (req, res) => {
 
-})
+});
 
 app.delete('/reservation/employee/i/:id', (req, res) => {
 
-})
+});
 
 app.get('/reservation/status', (req, res) => {
 
-})
+});
+
+//// EMPLOYEEE ROUTES///////
+
+
+////ADMIN ROUTESSS//////
 
 
 
