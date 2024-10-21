0a21eaa45ef4eb142d0728b9b8b37ade21489941
AbellanosaCarl <2201104567@student.buksu.edu.ph>
2024-10-21 08:23:31 +0800

    Admin routes akong ge butang

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 
 
+//DB CONNECTION
+require("./config/databaseConnection");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
+const sessionStore = new MongoDBStore({
+    //// dre ang pag store sa session /////
+})
 
 ///USER ROUTES//////
 
@@ -11,7 +17,7 @@ app.get('/reservation', (req, res) => {
 });
 const Id = req.body.Bookingid;
 
-user.deleteOne({ BookingId : Id}).then(item => {
+app.post("/CancelReservation", function (req, res) {
   res.send('Hello World!')
 });
 
@@ -32,12 +38,49 @@ app.delete('/reservation/employee/i/:id', (req, res) => {
 
 app.get('/reservation/status', (req, res) => {
 
-});
+})
 
 //// EMPLOYEEE ROUTES///////
 
 
 ////ADMIN ROUTESSS//////
+
+app.get("/goToAdminSection",function(req,res){
+  res.sendFile(__dirname + "/adminLogin.html");
+})
+
+app.post("/goToAdminSection",async function(req,res){
+  //validation sa admin///
+})
+
+app.post("/showAllBookings", function(req,res){
+/// view all boookings////
+})
+
+app.post("/addEmployee", function(req,res){
+  /// admin adds employee details to the system ////
+})
+app.get("/allEmployee", function(req,res){
+  /// admin view all employee details from the system ////
+})
+app.get("/Employee/i/:id", function(req,res){
+  /// admin view employee details by id from the system ////
+})
+app.delete("/deleteEmployee", function(req,res){
+  /// admin deletes employee details from the system ////
+})
+app.put("/updateEmployee", function(req,res){
+  /// admin update employee details from the system ////
+})
+
+
+app.get('/reservations/status', (req, res) => {
+  /// admin view all the reservstion status ///
+})
+
+app.get('/booking-history', (req, res) => {
+/// view the booking history ///
+})
 
 
 
