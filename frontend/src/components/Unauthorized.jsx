ce0e3aa7af55aa30770bc27bb27dae752db29203
HarmandDiolan <2201103870@student.buksu.edu.ph>
2024-12-14 04:06:33 +0800

    Naay nalimtan og butang

--- frontend/src/components/Unauthorized.jsx
+++ frontend/src/components/Unauthorized.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const Unauthorized = () => {
+    const navigate = useNavigate();
     return (
         <div>
             <h1>Unauthorized</h1>
             <p>You do not have permission to access this page.</p>
+            <button style={{textAlign: 'center', fontSize: '25px'}} onClick={() => navigate('/')}>Back to Home</button>
         </div>
     );
 };
